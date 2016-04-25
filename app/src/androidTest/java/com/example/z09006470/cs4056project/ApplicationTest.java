805b8bb372a1af9dfa448e22c4a62a41d79f9edd
MULVIHILL       SIOBHAN <MULVIHILL       SIOBHAN>
2016-04-25 17:32:59 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/example/z09006470/cs4056project/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.z09006470.cs4056project;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
